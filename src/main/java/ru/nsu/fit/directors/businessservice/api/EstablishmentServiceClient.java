7313af7f2694ba083359867f4c9793fe4bd9ca6d
Fagor <oleg-veber@mail.ru>
2024-02-29 01:14:12 +0700

    Add produces

--- src/main/java/ru/nsu/fit/directors/businessservice/api/EstablishmentServiceClient.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/api/EstablishmentServiceClient.java
@@ -3,6 +3,7 @@ package ru.nsu.fit.directors.businessservice.api;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +11,9 @@ import ru.nsu.fit.directors.businessservice.dto.response.ResponseShortEstablishm
 
 @FeignClient("establishment-service")
 public interface EstablishmentServiceClient {
-    @RequestMapping(method = RequestMethod.POST, value = "/internal/establishment")
+    @RequestMapping(method = RequestMethod.POST, value = "/internal/establishment", produces = MediaType.APPLICATION_JSON_VALUE)
     Long createEstablishment(@RequestParam Long ownerId);
 
-    @RequestMapping(method = RequestMethod.GET, value = "/internal/establishment/owner")
+    @RequestMapping(method = RequestMethod.GET, value = "/internal/establishment/owner", produces = MediaType.APPLICATION_JSON_VALUE)
     List<ResponseShortEstablishmentInfo> getEstablishmentsByOwner(@RequestParam Long ownerId);
 }
