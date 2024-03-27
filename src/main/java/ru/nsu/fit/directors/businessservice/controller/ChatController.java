3be14e8025c2dace5c8180286b8b790dc522c0ff
Fagorym <oleg-veber@mail.ru>
2024-03-27 23:43:12 +0700

    Add logging to messages

--- src/main/java/ru/nsu/fit/directors/businessservice/controller/ChatController.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/controller/ChatController.java
@@ -1,18 +1,16 @@
 package ru.nsu.fit.directors.businessservice.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 import ru.nsu.fit.directors.businessservice.dto.response.OutputMessageDto;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
@@ -20,13 +18,14 @@ public class ChatController {
     @MessageMapping("/send")
     @SendTo("/topic/greetings")
     public OutputMessageDto greeting(Message<String> message) throws Exception {
-        Thread.sleep(1000); // simulated delay
+        log.info("Received messsage {}", message);
         return new OutputMessageDto("Hello, " + HtmlUtils.htmlEscape(message.getPayload()) + "!", "ololo");
     }
 
     @MessageMapping("/send/{orderId}")
     @SendTo("/topic/{orderId}")
     public OutputMessageDto greeting(@DestinationVariable Long orderId) {
+        log.info("received order message {}", orderId);
         return new OutputMessageDto("Hello " + orderId + "Ololo", "ololo");
     }
 }
