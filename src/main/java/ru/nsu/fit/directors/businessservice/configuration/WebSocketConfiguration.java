c8fd3b4d38a24b7fcc4bdce7b2aca1b9db396825
Fagorym <oleg-veber@mail.ru>
2024-04-03 20:12:40 +0700

    Logging message headers

--- src/main/java/ru/nsu/fit/directors/businessservice/configuration/WebSocketConfiguration.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/configuration/WebSocketConfiguration.java
@@ -1,23 +1,11 @@
 package ru.nsu.fit.directors.businessservice.configuration;
 
-import java.util.Map;
-
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeFailureException;
-import org.springframework.web.socket.server.HandshakeHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-import ru.nsu.fit.directors.businessservice.model.BusinessUser;
 
 @Slf4j
 @Configuration
@@ -34,69 +22,10 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/business/chat")
             .setAllowedOriginPatterns("*")
-            .withSockJS()
-            .setInterceptors(httpSessionHandshakeInterceptor());
+            .withSockJS();
         registry.addEndpoint("/business/chat")
-            .setAllowedOriginPatterns("*")
-            .setHandshakeHandler(handshakeHandler());
+            .setAllowedOriginPatterns("*");
     }
 
-    @Bean
-    public HandshakeHandler handshakeHandler() {
-        return (request, response, wsHandler, attributes) -> {
-            if (request instanceof ServletServerHttpRequest servletServerRequest) {
-                HttpSession session = servletServerRequest.getServletRequest().getSession();
-                log.info(session.getId());
-                log.info(servletServerRequest.getHeaders().toString());
-                attributes.put("sessionId", session.getId());
-                attributes.put("principal", servletServerRequest.getPrincipal());
-                Object info = servletServerRequest.getPrincipal();
-                if (info instanceof BusinessUser user) {
-                    log.info(user.getUsername());
-                } else {
-                    log.info("No user");
-                }
-            }
-            return true;
-        };
-    }
-
-    @Bean
-    public HandshakeInterceptor httpSessionHandshakeInterceptor() {
-
-        return new HandshakeInterceptor() {
-            @Override
-            public boolean beforeHandshake(
-                ServerHttpRequest request,
-                ServerHttpResponse response,
-                WebSocketHandler wsHandler,
-                Map<String, Object> attributes
-            ) {
-                if (request instanceof ServletServerHttpRequest servletServerRequest) {
-                    HttpSession session = servletServerRequest.getServletRequest().getSession();
-                    log.info(session.getId());
-                    log.info(servletServerRequest.getHeaders().toString());
-                    attributes.put("sessionId", session.getId());
-                    attributes.put("principal", servletServerRequest.getPrincipal());
-                    Object info = servletServerRequest.getPrincipal();
-                    if (info instanceof BusinessUser user) {
-                        log.info(user.getUsername());
-                    } else {
-                        log.info("No user");
-                    }
-                }
-                return true;
-            }
-
-            @Override
-            public void afterHandshake(
-                ServerHttpRequest request,
-                ServerHttpResponse response,
-                WebSocketHandler wsHandler,
-                Exception exception
-            ) {
-            }
-        };
-    }
 }
 
--- src/main/java/ru/nsu/fit/directors/businessservice/controller/ChatController.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/controller/ChatController.java
@@ -1,14 +1,14 @@
 package ru.nsu.fit.directors.businessservice.controller;
 
 import java.util.List;
+import java.util.Map;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,8 +22,13 @@ public class ChatController {
     private final ChatService chatService;
 
     @MessageMapping("/send/{orderId}")
-    public void send(@DestinationVariable Long orderId, Message<MessageDto> message, HttpServletRequest request) {
+    public void send(
+        @DestinationVariable Long orderId,
+        Message<MessageDto> message,
+        @Headers Map<String, Object> messageHeaders
+    ) {
         log.info("received order message {}", orderId);
+        log.info("message headers {}", messageHeaders);
         chatService.save(message.getPayload(), orderId);
     }
 
