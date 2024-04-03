3e78675992c36155311bae9fc9c4153f4934241e
Fagorym <oleg-veber@mail.ru>
2024-04-03 19:31:34 +0700

    Some logging changes

--- src/main/java/ru/nsu/fit/directors/businessservice/controller/ChatController.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/controller/ChatController.java
@@ -2,17 +2,17 @@ package ru.nsu.fit.directors.businessservice.controller;
 
 import java.util.List;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.nsu.fit.directors.businessservice.dto.request.MessageDto;
-import ru.nsu.fit.directors.businessservice.dto.response.OutputMessageDto;
 import ru.nsu.fit.directors.businessservice.service.ChatService;
 
 @Slf4j
@@ -22,7 +22,9 @@ public class ChatController {
     private final ChatService chatService;
 
     @MessageMapping("/send/{orderId}")
-    public void send(@DestinationVariable Long orderId, Message<MessageDto> message) {
+    public void send(@DestinationVariable Long orderId, Message<MessageDto> message, HttpServletRequest request) {
+        log.info(request.toString());
+        log.info(SecurityContextHolder.getContext().toString());
         log.info("received order message {}", orderId);
         chatService.save(message.getPayload(), orderId);
     }
