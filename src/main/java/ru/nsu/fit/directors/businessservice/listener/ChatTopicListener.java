f7b12f23e41f5b572c31c2e8a98258b4a3f09a07
Fagorym <oleg-veber@mail.ru>
2024-04-03 21:41:06 +0700

    Add trusted packages

--- src/main/java/ru/nsu/fit/directors/businessservice/listener/ChatTopicListener.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/listener/ChatTopicListener.java
@@ -3,12 +3,14 @@ package ru.nsu.fit.directors.businessservice.listener;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ru.nsu.fit.directors.businessservice.event.UserMessageEvent;
 import ru.nsu.fit.directors.businessservice.service.ChatService;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @ParametersAreNonnullByDefault
@@ -18,6 +20,7 @@ public class ChatTopicListener {
 
     @KafkaHandler
     public void handleUserMessage(UserMessageEvent userMessageEvent) {
+        log.info("Receive user message {}", userMessageEvent);
         chatService.handleMessage(userMessageEvent);
     }
 }
