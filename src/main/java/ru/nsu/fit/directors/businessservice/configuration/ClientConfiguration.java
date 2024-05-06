064e70880cdd65ea1e3e67f68d0aeeb8dd3871c7
Fagorym <oleg-veber@mail.ru>
2024-05-07 00:39:22 +0700

    Create encoder

--- src/main/java/ru/nsu/fit/directors/businessservice/configuration/ClientConfiguration.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/configuration/ClientConfiguration.java
@@ -1,8 +1,13 @@
 package ru.nsu.fit.directors.businessservice.configuration;
 
 import feign.Logger;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ClientConfiguration {
@@ -10,4 +15,12 @@ public class ClientConfiguration {
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
     }
+
+    @Bean
+    public Encoder multipartFormEncoder() {
+        return new SpringFormEncoder(
+            new SpringEncoder(() -> new HttpMessageConverters(new RestTemplate().getMessageConverters()))
+        );
+    }
+
 }
