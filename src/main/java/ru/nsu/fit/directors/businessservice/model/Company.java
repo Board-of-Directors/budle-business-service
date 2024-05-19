50194879f7908c6c2c51e0f398fa449e73c71840
Fagorym <oleg-veber@mail.ru>
2024-05-19 21:02:15 +0700

    Delete notification cascade

--- src/main/java/ru/nsu/fit/directors/businessservice/model/Company.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/model/Company.java
@@ -1,11 +1,13 @@
 package ru.nsu.fit.directors.businessservice.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,9 +26,13 @@ public class Company {
     @ManyToOne
     @JoinColumn(name = "business_user_id")
     private BusinessUser businessUser;
+
     @ManyToMany
     @JoinTable(name = "company_workers",
         joinColumns = @JoinColumn(name = "company_id"),
         inverseJoinColumns = @JoinColumn(name = "worker_id"))
     private List<BusinessUser> workers;
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private List<Notification> notifications;
 }
