60177650967959e2e9423f76692579c9b64f6fc3
Fagorym <oleg-veber@mail.ru>
2024-05-23 10:47:52 +0700

    Add new worker validation

---
+++ src/main/java/ru/nsu/fit/directors/businessservice/exceptions/WrongActionException.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.directors.businessservice.exceptions;
+
+public class WrongActionException extends BaseException {
+    public WrongActionException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/nsu/fit/directors/businessservice/facade/WorkerFacadeImpl.java
+++ src/main/java/ru/nsu/fit/directors/businessservice/facade/WorkerFacadeImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import ru.nsu.fit.directors.businessservice.controller.AddWorkerRequest;
 import ru.nsu.fit.directors.businessservice.dto.request.InviteWorkerRequest;
 import ru.nsu.fit.directors.businessservice.dto.request.RequestOptionDto;
+import ru.nsu.fit.directors.businessservice.exceptions.WrongActionException;
 import ru.nsu.fit.directors.businessservice.mapper.OptionConverter;
 import ru.nsu.fit.directors.businessservice.model.AvailableOption;
 import ru.nsu.fit.directors.businessservice.model.BusinessUser;
@@ -37,18 +38,20 @@ public class WorkerFacadeImpl implements WorkerFacade {
         Company company = companyService.getById(establishmentId);
         employeeService.validateWorker(company, Option.DELETE_WORKERS);
         BusinessUser worker = businessUserService.getById(workerId);
+        if (Objects.equals(worker.getId(), company.getBusinessUser().getId())) {
+            throw new WrongActionException("Нельзя удалить владельца из заведения");
+        }
         optionService.delete(worker, company);
     }
 
     @Override
     public void inviteWorker(InviteWorkerRequest inviteWorkerRequest) {
-        BusinessUser active = employeeService.getLoggedInUser();
         employeeService.validateOwner(inviteWorkerRequest.establishmentId());
         BusinessUser businessUser = businessUserService.getByToken(inviteWorkerRequest.token());
-        if (Objects.equals(active.getId(), businessUser.getId())) {
-        }
-
         Company company = companyService.getById(inviteWorkerRequest.establishmentId());
+        if (Objects.equals(businessUser.getId(), company.getBusinessUser().getId())) {
+            throw new WrongActionException("Нельзя добавить владельца заведения");
+        }
         List<AvailableOption> availableOptions = inviteWorkerRequest.options()
             .stream()
             .filter(RequestOptionDto::isEnabled)
@@ -62,6 +65,9 @@ public class WorkerFacadeImpl implements WorkerFacade {
         employeeService.validateOwner(addWorkerRequest.establishmentId());
         BusinessUser businessUser = businessUserService.getById(addWorkerRequest.workerId());
         Company company = companyService.getById(addWorkerRequest.establishmentId());
+        if (Objects.equals(businessUser.getId(), company.getBusinessUser().getId())) {
+            throw new WrongActionException("Нельзя добавить владельца заведения");
+        }
         List<AvailableOption> availableOptions = addWorkerRequest.options()
             .stream()
             .filter(RequestOptionDto::isEnabled)
